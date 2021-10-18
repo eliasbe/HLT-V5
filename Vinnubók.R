777333e10a8c08d2bbd2c83ab2e7d358745e56c4
eliasbe <eliaseinars@gmail.com>
2021-10-18 15:06:23 +0000

    Bætti við gagnakönnun

--- Vinnubók.R
+++ Vinnubók.R
@@ -7,22 +7,38 @@ library(GGally)
 set.seed(11)
 
 
-data <- read.table("gagnasafn_endurmat2017_litid.csv", header = T, sep = ",") #, col.names = dalkar
+data <- data.frame(read.table("gagnasafn_endurmat2017_litid.csv", header = T, sep = ","))
 
-data[ ,"kdagur"] <- as.Date(data[ ,"kdagur"])
-data[ ,"teg_eign"] <- as.factor(data[ ,"teg_eign"])
-data[ ,"svfn"] <- as.factor(data[ ,"svfn"])
+summary(data)
+str(data)
+
+data[ ,"kdagur"] <- as.Date(data[ ,"kdagur"]) # Kaupdagur sem dagsetning
+data[ ,"teg_eign"] <- as.factor(data[ ,"teg_eign"]) # Tegund eignar sem flokkur
+data[ ,"svfn"] <- as.factor(data[ ,"svfn"]) # Svæði sem flokkur
+
 # Ath að í verkefni segir að "lyfta" sé binary en í gögnum virðist hún vera fjöldi lyfta.
 data[ ,"lyfta"] <- data[,"lyfta"]>0 # Kannski halda, kannski sleppa
 
+# Kannski hafa stig10 logical 10 eða ekki 10.
+data[ ,"matssvaedi"] <- as.factor(data[ ,"matssvaedi"]) # Staðsetning sem flokkur
+data[ ,"undirmatssvaedi"] <- as.factor(data[ ,"undirmatssvaedi"]) # Undirstaðsetning sem flokkur
+data[ ,"ibteg"] <- as.factor(data[ ,"ibteg"]) # Tegund íbúðar sem flokkur (ath, bara tveir flokkar og stendur numerical í verkefnalýsingu)
+
 summary(data)
-
 str(data)
+typeof(data) # afhverju list en ekki data frame?
 
-data[data["nuvirdi"]==203781]
+group_by(data, ibteg, teg_eign) %>% count()?
+# Sleppa ibteg
+data <- subset(data, select = ibteg)
 
-# Spurning um að hafa svfn (póstnúmer? svæðisnúmer?) flokkunarbreytu
-# lyfta logical - nei virðist vera fjöldi lyfta
-# matssvæði og undirmatssvæði categorical
-# ibteg, numerical?
+# Spurning um að einfalda teg_eign í Ibudareign vs. rest (Einbyli, par- og raðhús)
+# Gera betur
+# data[data["teg_eign"]  == "Einbylishus" || data["teg_eign"] == "Parhus" || data["teg_eign"] == "Radhus"] = "Sérbýli"
+
+
+# ggpairs(data, cardinality_threshold = 168)
+# ggpairs(data[ , -which(names(data) %in% c("svfn", "matssvaedi", "undirmatssvaedi"))])
+
+# PLOT RESIDUAL Á MÓTI FITTED
 
