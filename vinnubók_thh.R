f32826f304561d2e663884e411b8bc2f766b79b2
Þórhallur Auður Helgason <thorhallurh@gmail.com>
2021-10-19 16:44:55 +0000

    Vinna meira

--- vinnubók_thh.R
+++ vinnubók_thh.R
@@ -6,15 +6,27 @@ library(tidyverse)
 library(GGally)
 set.seed(11)
 
+sizeTraining = floor(0.75 * nrow(savings))
+trainingSampleRowId <- sample(1:nrow(savings), size = sizeTraining, replace = F)
+trainingSavings <- savings[trainingSampleRowId, ]
+testSavings <- savings[-trainingSampleRowId, ]
 
-data <- data.frame(read.table("gagnasafn_endurmat2017_litid.csv", header = T, sep = ","))
+data_big <- data.frame(read.table("gagnasafn_endurmat2017_litid.csv", header = T, sep = ","))
 
-summary(data)
-str(data)
+summary(data_big)
+str(data_big)
+
+# (i) Miðbær frá Bræðraborgarstíg að Tjörn; (ii) Melar að sjó; (iii) Háleiti/Skeifa;
+# (iv) Hólar, Berg; (v) Réttarholt
+sublocations = c(25, 70, 91, 160, 281)
+
+# Við getum klippt út 'svfn' breytuna hér, hún er eins hjá öllum.
+data = data_big[data_big$matssvaedi == sublocations, ]
+# ATH, tökum út SVFN því öll gögn taka sama gildi þar, 0
+data <- subset( data, select = -svfn )
 
 data[ ,"kdagur"] <- as.Date(data[ ,"kdagur"]) # Kaupdagur sem dagsetning
 data[ ,"teg_eign"] <- as.factor(data[ ,"teg_eign"]) # Tegund eignar sem flokkur
-data[ ,"svfn"] <- as.factor(data[ ,"svfn"]) # Svæði sem flokkur
 
 # Ath að í verkefni segir að "lyfta" sé binary en í gögnum virðist hún vera fjöldi lyfta.
 data[ ,"lyfta"] <- data[,"lyfta"]>0 # Kannski halda, kannski sleppa
@@ -24,28 +36,30 @@ data[ ,"matssvaedi"] <- as.factor(data[ ,"matssvaedi"]) # Staðsetning sem flokk
 data[ ,"undirmatssvaedi"] <- as.factor(data[ ,"undirmatssvaedi"]) # Undirstaðsetning sem flokkur
 data[ ,"ibteg"] <- as.factor(data[ ,"ibteg"]) # Tegund íbúðar sem flokkur (ath, bara tveir flokkar og stendur numerical í verkefnalýsingu)
 
-# (i) Miðbær frá Bræðraborgarstíg að Tjörn; (ii) Melar að sjó; (iii) Háleiti/Skeifa;
-# (iv) Hólar, Berg; (v) Réttarholt
-sublocations = c(25, 70, 91, 160, 281)
-
-sample_data = data[data$matssvaedi == sublocations, ]
-
-
-summary(sample_data)
+summary(data)
 str(data)
-typeof(data) # afhverju list en ekki data frame?
+
+
+sizeTraining = floor(0.75 * nrow(data))
+trainingSampleRowId <- sample(1:nrow(data), size = sizeTraining, replace = F)
+train_data <- data[trainingSampleRowId, ]
+test_data <- data[-trainingSampleRowId, ]
 
 group_by(data, ibteg, teg_eign) %>% count()
-# Sleppa ibteg
-data <- subset(data, select = ibteg)
+#data <- subset(data, select = -ibteg)
 
-# Spurning um að einfalda teg_eign í Ibudareign vs. rest (Einbyli, par- og raðhús)
-# Gera betur
-# data[data["teg_eign"]  == "Einbylishus" || data["teg_eign"] == "Parhus" || data["teg_eign"] == "Radhus"] = "Sérbýli"
+lm.first = lm(nuvirdi ~ ., data = train_data)
+summary(lm.first)
+summary(residuals(lm.first)^2)
 
+nums <- unlist(lapply(data, is.numeric))  
+ggpairs(data[,nums])
 
-# ggpairs(data, cardinality_threshold = 168)
-# ggpairs(data[ , -which(names(data) %in% c("svfn", "matssvaedi", "undirmatssvaedi"))])
-
-# PLOT RESIDUAL Á MÓTI FITTED
-
+pm <- ggpairs(
+  data[,nums],
+  upper = list(continuous = wrap("density", alpha = 0.5), combo = "box"),
+  lower = list(continuous = wrap("points", alpha = 0.3,    size=0.1), 
+               combo = wrap("dot", alpha = 0.4,            size=0.2) ),
+  title = "Diamonds"
+)
+pm
